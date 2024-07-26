272d4b7c93aa3ae76f0497980b1d5551933cb802
vo huu nhan <vohuunhan1310@gmail.com>
2024-07-27 00:52:45 +0700

    fix: api

--- src/app/api/list_pet/route.ts
+++ src/app/api/list_pet/route.ts
@@ -16,23 +16,8 @@ export async function POST(req: NextRequest) {
       args_base64: 'e30=',
       finality: "optimistic",
     });
-    console.log(JSON.parse(Buffer.from((await allpets).result).toString()))
   const petList = JSON.parse(Buffer.from((await allpets).result).toString()).filter((pet:any) => pet.owner_id == accountID )
-  //console.log(petList)
-  const listInfoPet:any = [];
-  for(let i=0; i < petList.length; i++){
-    const infoPet = provider.query<CodeResult>({
-      request_type: "call_function",
-      account_id: process.env.CONTRACT_GAME,
-      method_name: process.env.METHOD_GET_PET_ID,
-      args_base64: btoa(`{"pet_id": ${petList[i].pet_id}}`),
-      finality: "optimistic",
-    })
-    const result = JSON.parse(Buffer.from((await infoPet).result).toString());
-    //console.log(result)
-    listInfoPet.push(result);
-  }
-  return  NextResponse.json(listInfoPet,{ status: 200 })
+  return  NextResponse.json(petList,{ status: 200 })
 }
 
 export async function GET(req: Request) {
